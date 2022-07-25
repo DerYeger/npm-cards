c9017d82c8999ef16c4300a02976d89be19e749a
Jan Müller <janmueller3698@gmail.com>
2022-07-25 14:58:50 +0200

    feat(frontend): add view counter

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,23 @@
+import PageViews from '@yeger/page-views'
 import type { FC } from 'react'
+import { useEffect, useState } from 'react'
 
 import '@/components/Footer.css'
 
 const Footer: FC = () => {
+  const [pageViews, setPageViews] = useState<number>()
+
+  useEffect(() => {
+    PageViews.getViews().then(setPageViews)
+  })
+
+  const pageViewElement =
+    pageViews !== undefined ? (
+      <span>{pageViews} views</span>
+    ) : (
+      <span>Loading...</span>
+    )
+
   return (
     <footer>
       <a
@@ -13,6 +28,7 @@ const Footer: FC = () => {
         GitHub
       </a>
       <span>Made by Jan with 🐹 and ⚛️</span>
+      {pageViewElement}
     </footer>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
+import PageViews from '@yeger/page-views'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from '@/App'
+
 import '@/index.css'
+
+PageViews.autoSubmitViews()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
